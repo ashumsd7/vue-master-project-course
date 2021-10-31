d8e0c916c510348ab868e51f182142403d48101b
ashumsd7 <aat8001@gmail.com>
2021-10-31 10:55:23 +0530

    store added with modules structure

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import router from "./router.js"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+import store from './store/index'
 import App from './App.vue'
 
 const app= createApp(App)
 app.use(router);
+app.use(store)
 app.mount('#app');
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -9,13 +9,23 @@
     <router-link to="/register" >Register a Coach</router-link>
 </div>
     LIST OF COACHES
+
+
+    <ul>
+        <li v-for="coach in filteredCoaches" :key="coach.id"> {{coach.firstName}}</li> 
+    </ul>
 </section>
 
 </template>
 
 <script>
     export default {
-        
+        computed:{
+            filteredCoaches(){
+                //this is name space accessing of indexjs state
+                return this.$store.getters['coaches/coaches']
+            }
+        }
     }
 </script>
 
