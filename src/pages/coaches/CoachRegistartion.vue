4c569648e5895fb1fa5775b9069e020cf3d5fc21
ashumsd7 <aat8001@gmail.com>
2021-11-07 09:58:29 +0530

    coach Registration page added

--- src/pages/coaches/CoachRegistartion.vue
+++ src/pages/coaches/CoachRegistartion.vue
@@ -1,12 +1,19 @@
 <template>
-    <div>
-registartiopn
-    </div>
+ <section>
+     <base-card>
+    
+     <h2>Register as a Coach!!</h2>
+     <coach-form></coach-form>
+      </base-card>
+ </section>
 </template>
 
 <script>
+import CoachForm from '../../components/coaches/CoachForm.vue'
     export default {
-        
+        components:{
+            CoachForm
+        }
     }
 </script>
 
