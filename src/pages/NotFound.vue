aee16781aecf8f553fb3ac0b83a67cfcff563346
ashumsd7 <aat8001@gmail.com>
2021-11-10 23:48:21 +0530

    404 Not Found Page Added

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
-404
-    </div>
+        <base-card>
+<img class="img-fluid" src="https://static.vecteezy.com/system/resources/previews/001/857/111/non_2x/error-404-page-not-found-landing-page-concept-for-mobile-and-pc-free-vector.jpg" alt="" sizes="" srcset="">
+<router-link class="d-block w-50 m-auto btn btn-primary" to='/'> Go to Home Page</router-link>
+ </base-card>   </div>
 </template>
 
 <script>
